78dc87ef6cf16b9dc557530d4be95bc5ffd06ef0
LeoNull <820073786@qq.com>
2016-07-25 17:37:01 +0800

    jj

--- imooc/baidu_spider/spider_main.py
+++ imooc/baidu_spider/spider_main.py
@@ -1,11 +1,13 @@
 #coding:utf-8
-from baidu_spider import url_manager
+from baidu_spider import url_manager, html_downloader, html_outputer
 
 
 class SpiderMain(object):
     
     def _init_(self):
         self.urls=url_manager.__name__
+        self.downloader=html_downloader.__name__
+        self.parser=html_outputer.__name__
         
     def crawl(self, root_url):
         pass
