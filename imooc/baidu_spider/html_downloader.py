4c9bce7cc971488e1c7544d3e9531a456ed3e04a
LeoNull <820073786@qq.com>
2016-07-28 17:18:34 +0800

    success crawl baidu

--- imooc/baidu_spider/html_downloader.py
+++ imooc/baidu_spider/html_downloader.py
@@ -1,5 +1,20 @@
-'''
-Created on 2016年7月25日
 
-@author: lei.chen
-'''
+import urllib2
+
+
+class HtmlDownload(object):
+    
+    
+    def download(self,url):
+        if url is None:
+            return None
+        response=urllib2.urlopen(url)
+        
+        if response.getcode()!=200:
+            return None
+        return response.read()
+    
+    
+
+
+
