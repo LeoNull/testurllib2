7eae054bdf62d68a55badff2067783ce4028a542
LeoNull <820073786@qq.com>
2016-08-02 14:19:09 +0800

    reptile success!

--- imooc/test/reptile.py
+++ imooc/test/reptile.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
-# coding=utf-8
+# coding=gbk
 import re 
-import os
+
 import time
 import urllib2
 import urllib
 
 def get_urls(url):
     html = urllib2.urlopen(url).read()
-    picurl = re.compile(r'<img alt=".+?" src="(.+?)" /><br />')
+   
+    picurl = re.compile(r'http://pic\.meizitu\.com/wp-content/uploads.+\.jpg')
     picts = picurl.findall(html)
+    
     for urls in picts:
         try:
+            print urls
             name = re.sub(':', '_', time.ctime()[10:19] + '.jpg')
             print name
             urllib.urlretrieve(urls, 'E:/Github/meizitu/tu/' + name)
@@ -21,18 +24,18 @@ def get_urls(url):
 
 def get_href(page):
     s = urllib2.urlopen(page)
-    hrefs = re.compile(r'<h2><a href="(.+?)".+?</a></h2>')
+    hrefs = re.compile(r'http://www\.meizitu\.com/a/\d+\.html')
     s = s.read()
     links = hrefs.findall(s)
-    return links
+    linked=links[1:10]
+    return linked
 
 def main():
-    for i in xrange(1, 67):
+    for i in xrange(1, 2):
         a = 'http://www.meizitu.com/a/list_1_%d.html' % i
         href = get_href(a)
         time.sleep(1)
         for j in href:
             get_urls(j)
 if __name__ == '__main__':
-    os.chdir('E:/Github/meizitu/tu/') 
     main()
